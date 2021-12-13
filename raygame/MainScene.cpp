3ddb58e88d83765a6b96ed9bc98611845946ef3f
Peyton <peytonsemail2000@gmail.com>
2021-12-13 16:44:34 -0600

    Player has movement

--- raygame/MainScene.cpp
+++ raygame/MainScene.cpp
@@ -1,14 +1,13 @@
 #include "MainScene.h"
-#include "Actor.h"
 #include "SpriteComponent.h"
 #include "MoveComponent.h"
 #include "Transform2D.h"
+#include "Player.h"
 
 void MainScene::start()
 {
-	Actor* player = new Actor(50, 50, "Player");
-	player->addComponent(new SpriteComponent("Images/player.png"));
-	player->addComponent(new MoveComponent());
+	Player* player = new Player(50, 50, "Player");
 	player->getTransform()->setScale({ 50, 50 });
+
 	addActor(player);
 }
